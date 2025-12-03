995ab891c56ee7a862570085ae1cb4b17b72a828
Shobhit rajvanshi <shobhitraj1998@gmail.com>
2025-12-04 00:07:05 +0530

    feat: added header component

--- src/components/common/Header.jsx
+++ src/components/common/Header.jsx
@@ -1,7 +1,8 @@
-import { useEffect } from "react";
-import { useDispatch } from "react-redux";
+import { useEffect, useRef, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
 import { useNavigate } from "react-router-dom";
-import { onAuthStateChanged } from "firebase/auth";
+import { onAuthStateChanged, signOut } from "firebase/auth";
+import { FaBell } from "react-icons/fa";
 
 import { LOGO } from "../../utils/constants";
 import { addUser, removeUser } from "../../utils/redux/userSlice";
@@ -10,6 +11,7 @@ import { auth } from "../../utils/firbase";
 const Header = () => {
   const dispatch = useDispatch();
   const navigate = useNavigate();
+  const user = useSelector((store) => store.user);
 
   useEffect(() => {
     const unsubscribe = onAuthStateChanged(auth, (user) => {
@@ -34,10 +36,63 @@ const Header = () => {
     return () => unsubscribe();
   }, []);
 
+  const handleSignOut = () => {
+    signOut(auth)
+      .then(() => {})
+      .catch((error) => {
+        navigate("/error");
+      });
+  };
+
   return (
-    <div className="absolute w-screen px-8 py-2 bg-gradient-to-b from-black z-10">
-      <img className="w-44 mx-auto md:mx-0" src={LOGO} alt="logo" />
-    </div>
+    <header className="fixed top-0 left-0 w-full z-50 px-6 py-4 bg-gradient-to-b from-black/90 to-transparent flex items-center justify-between">
+      {/* Logo */}
+      <img src={LOGO} alt="Netflix-logo" className="w-28 cursor-pointer" />
+
+      {user && (
+        <nav className="flex items-center gap-8 text-gray-300 font-medium">
+          <ul className="hidden md:flex gap-6 text-sm">
+            <li className="cursor-pointer hover:text-white transition">Home</li>
+            <li className="cursor-pointer hover:text-white transition">
+              TV Shows
+            </li>
+            <li className="cursor-pointer hover:text-white transition">
+              Movies
+            </li>
+            <li className="cursor-pointer hover:text-white transition">
+              New & Popular
+            </li>
+            <li className="cursor-pointer hover:text-white transition">
+              My List
+            </li>
+          </ul>
+
+          <FaBell className="hidden md:block cursor-pointer hover:text-white transition" />
+
+          {/* Profile */}
+          <div className="flex justify-center items-center">
+            <div className="flex justify-center items-center gap-2">
+              <img
+                src={user?.photoURL}
+                alt="user"
+                className="w-8 h-8 rounded cursor-pointer"
+              />
+
+              <p className="text-white">{user.displayName}</p>
+            </div>
+
+            <button
+              onClick={handleSignOut}
+              className="ml-2 bg-red-600 hover:bg-red-700 active:bg-red-800 text-white px-4 py-2 rounded-md font-semibold shadow-sm transition-all focus:outline-none focus:ring-2 focus:ring-red-500 focus:ring-offset-2"
+              aria-label="Sign out"
+              title="Sign out"
+            >
+              Sign Out
+            </button>
+          </div>
+        </nav>
+      )}
+    </header>
   );
 };
 
