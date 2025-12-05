e4b35edb3e64a0a35f761f43b7c80c33e1be1098
Shobhit rajvanshi <shobhitraj1998@gmail.com>
2025-12-05 22:20:24 +0530

    feat: updated ui

--- src/components/browse/secondaryContainer/MovieCard.js
+++ src/components/browse/secondaryContainer/MovieCard.js
@@ -3,8 +3,12 @@ import { IMG_CDN_URL } from "../../../utils/constants";
 const MovieCard = ({ posterPath }) => {
   if (!posterPath) return null;
   return (
-    <div className="w-36 md:w-48 pr-4">
-      <img alt="Movie Card" src={IMG_CDN_URL + posterPath} />
+    <div className="relative w-32 sm:w-36 md:w-48 cursor-pointer transition-transform duration-300 ease-in-out hover:scale-110 hover:-translate-y-2 hover:z-50">
+      <img
+        className="rounded-md shadow-md hover:shadow-2xl transition-all duration-300"
+        alt="Movie Card"
+        src={IMG_CDN_URL + posterPath}
+      />
     </div>
   );
 };
