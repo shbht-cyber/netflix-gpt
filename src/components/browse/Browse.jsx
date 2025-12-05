0da67da8acdf73dc804cbb08afadd710640e1e2c
Shobhit rajvanshi <shobhitraj1998@gmail.com>
2025-12-05 22:39:33 +0530

    feat: added other movie categories

--- src/components/browse/Browse.jsx
+++ src/components/browse/Browse.jsx
@@ -3,13 +3,17 @@ import useNowPlayingMovies from "../../hooks/useNowPlayingMovies";
 import usePopularMovies from "../../hooks/usePopularMovies";
 import MainContainer from "./mainContainer/MainContainer";
 import SecondaryContainer from "./secondaryContainer/SecondaryContainer";
+import useTopRatedMovies from "../../hooks/useTopRatedMovies";
+import useUpcomingMovies from "../../hooks/useUpcomingMovies";
 
 const Browse = () => {
   useNowPlayingMovies();
   usePopularMovies();
+  useTopRatedMovies();
+  useUpcomingMovies();
 
   return (
-    <div>
+    <div className="overflow-x-hidden no-scrollbar">
       <Header />
 
       <MainContainer />
