d6875b52ea8f990e23a68905ea5a9f9c46fc56d2
Shobhit rajvanshi <shobhitraj1998@gmail.com>
2025-12-07 17:43:04 +0530

    fix: bg image in case of gpt search

--- src/components/browse/gpt/GptSearch.js
+++ src/components/browse/gpt/GptSearch.js
@@ -6,11 +6,12 @@ import { BG_URL } from "../../../utils/constants";
 const GPTSearch = () => {
   return (
     <div
+      className="min-h-screen bg-cover bg-center bg-no-repeat"
       style={{
         backgroundImage: `url('${BG_URL}')`,
       }}
     >
-      <div className="">
+      <div className="min-h-screen backdrop-brightness-50">
         <GptSearchBar />
         <GptMovieSuggestions />
       </div>
